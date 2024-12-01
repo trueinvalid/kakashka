7268806d65d6d6e74b1747cdd27a8dcf3060e932
trueinvalid <kk9shheey@gmail.com>
2024-12-01 10:27:17 +0600

    Создали новый файл питона
    Тема словарь, список, кортеж

---
+++ index/index.py
@@ -0,0 +1,42 @@
+#  fruits1 = {"банан","яблоко","мандарин"} #список
+#  print(fruits1)
+
+#  colors = ("красный","белый","зеленый") #кортеж
+#  print(colors)
+
+#  students = {
+#      "Алиса":16,
+#     "Сири":10,
+#      "Макссим": 15
+# }
+
+#  print(students)
+#  students["Максим"] = 2 // #изменение по ключю
+#  print(students)
+#  del students["Максим"] // удаление
+#  print(students)
+
+
+fruits = {
+    "banana":"yellow",
+    "apple":"red",
+    "mango":"green",
+    "orange":"orange",
+};
+
+fruits["mango"] = "red",
+print(fruits)*
+del  fruits["orange"]
+print(fruits)
+
+bestNumbers = {
+    5 : "отлично"
+    13 : "что то ожидается"
+}
+print(bestNumbers[13])
+haha = {
+    True: "Это правда"
+    False: "Это ложь"
+}
+print(haha[False])
+
